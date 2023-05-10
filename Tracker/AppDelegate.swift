37a2925cc0d5f7dde79135f3551ef6129f356be0
Vsevolod Tsarev <vsevolodtsarev.work@gmail.com>
2023-05-10 19:49:47 +0700

    CoreData implementation

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -6,11 +6,22 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
+    // MARK: CoreData
     
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreDataModel")
+        container.loadPersistentStores { (storeDescription, error)  in
+            if let error = error as? NSError? {
+                assertionFailure("persistent container error - \(String(describing: error))")
+            }
+        }
+        return container
+    }()
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -32,11 +43,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-    
-    
 }
 
